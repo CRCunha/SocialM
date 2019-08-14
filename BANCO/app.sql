df45095dbec3ac853d2a27b22bfa3d4fb5d4593f
#! <cristianraffi@bol.com.br>
2019-08-14 08:07:27 -0300

    atualização banco

--- BANCO/app.sql
+++ BANCO/app.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Tempo de geração: 13/08/2019 às 13:40
+-- Tempo de geração: 14/08/2019 às 13:06
 -- Versão do servidor: 10.3.15-MariaDB
 -- Versão do PHP: 7.3.6
 
@@ -31,7 +31,8 @@ SET time_zone = "+00:00";
 CREATE TABLE `mensagens` (
   `mensagem` varchar(255) NOT NULL,
   `nome` varchar(255) NOT NULL,
-  `dataMsg` datetime NOT NULL
+  `dataMsg` datetime NOT NULL,
+  `avatar` varchar(2000) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -54,9 +55,10 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`nome`, `email`, `senha`, `id`, `background`, `avatar`) VALUES
-('Carol', 'carolroluz@hotmail.com', '742a90d6f000d65dda5433aedbd9524c112d8371', 1, 'https://images.pexels.com/photos/2722990/pexels-photo-2722990.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940', 'https://i.imgur.com/nbXuzbx.jpg'),
+('Cristian', 'carolroluz@hotmail.com', '742a90d6f000d65dda5433aedbd9524c112d8371', 1, 'https://image.freepik.com/vetores-gratis/modelo-de-plano-de-fundo-branco-infografico-hexagonal_1017-19276.jpg', 'https://i.imgur.com/vKSf7z5.png'),
 ('Lucas', 'lucasguidotthi@hotmal.com', 'fe703d258c7ef5f50b71e06565a65aa07194907f', 2, 'https://i.imgur.com/nNyr7SO.png', 'https://i.imgur.com/E9Gk0e2.png'),
-('Cristian', 'contato@cristiancunha.me', '80d616a7b726e7b08c12194e789ea87f8ee09b37', 3, 'https://images.pexels.com/photos/414171/pexels-photo-414171.jpeg?cs=srgb&dl=adventure-calm-clouds-414171.jpg&fm=jpg', 'https://png.pngtree.com/element_our/png/20181214/mountain-logo-graphic-design-template-vector-illustration-png_269527.jpg');
+('Cristian', 'contato@cristiancunha.me', '80d616a7b726e7b08c12194e789ea87f8ee09b37', 3, 'https://image.freepik.com/vetores-gratis/modelo-de-plano-de-fundo-branco-infografico-hexagonal_1017-19276.jpg', 'https://i.imgur.com/vKSf7z5.png'),
+('Teste', 'teste@teste.com', '6ba19813d8fe1145922b11ba580e477f6ea0dfb7', 11, 'https://image.freepik.com/vetores-gratis/modelo-de-plano-de-fundo-branco-infografico-hexagonal_1017-19276.jpg', 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSDtcZZoolUHT2_OoEKXqAn006-fGBvWEenEpwOaNViaqVoH1LO');
 
 --
 -- Índices de tabelas apagadas
@@ -76,7 +78,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT de tabela `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
